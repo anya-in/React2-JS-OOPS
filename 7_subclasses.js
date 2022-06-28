a9287efd5795e14d60fc92169ed90ca833a6c44e
Anya <79497125+anya-in@users.noreply.github.com>
2022-06-28 16:43:47 +0530

    Add files via upload

---
+++ 7_subclasses.js
@@ -0,0 +1,21 @@
+class Book {
+  constructor(title, author, year) {
+    this.title = title;
+    this.author = author;
+    this.year = year;
+  }
+
+  getSummary() {
+    return `${this.title} was written by ${this.author} in ${this.year}`;
+  }
+}
+//Magazine Subclass
+class Magazine extends Book {
+  constructor(title, author, year, month) {
+    super(title, author, year);
+    this.month = month;
+  }
+}
+//Instantiate magazine
+const mag1 = new Magazine('Mag One', 'Sara Inara', '2019', 'Jan');
+console.log(mag1.getSummary());
